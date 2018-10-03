ce07725caced4acd460ae43f9824777d0c5c5385
Константин Шкредов <kostig51@gmail.com>
2018-10-03 12:52:29 +0300

    Привязка поля

--- lib/model/doctrine/ActivityFields.class.php
+++ lib/model/doctrine/ActivityFields.class.php
@@ -195,13 +195,13 @@ class ActivityFields extends BaseActivityFields
         $data_array = array();
         if (!$field_data) {
             $field_data = new ActivityFieldsValues();
-        }
 
+            $data_array['field_id'] = $field_id;
+        }
 
         $data_array['year'] = $year;
         $data_array['q'] = $quarter;
 
-        //$data_array['field_id'] = $field_id;
         $data_array['val'] = $field_value;
         $data_array['dealer_id'] = $user->getDealer()->getId();
         //$data_array['user_id'] = $user->getId();
