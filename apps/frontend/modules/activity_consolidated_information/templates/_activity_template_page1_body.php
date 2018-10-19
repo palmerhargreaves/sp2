eab389efa7a917ecae5ab14e1a35603e6d42a526
Константин Шкредов <kostig51@gmail.com>
2018-10-19 18:16:59 +0300

    Правки

--- apps/frontend/modules/activity_consolidated_information/templates/_activity_template_page1_body.php
+++ apps/frontend/modules/activity_consolidated_information/templates/_activity_template_page1_body.php
@@ -7,91 +7,111 @@
  */
 ?>
 
-<main id="d-content">
+<div id="d-wrap" class="page-wrap is-flexbox">
+    <header id="d-header">
+        <div class="d-grid">
 
-    <div class="d-grid">
-
-        <nav class="nav_quarts">
-            <ul class="is-flexbox is-flexbox_justify">
-                <li class="active">1 квартал</li>
-                <li class="active">2 квартал</li>
-                <li>3 квартал</li>
-                <li>4 квартал</li>
-            </ul>
-        </nav>
-
-        <h2 class="h1 d-ttu fw_400">Имиджевая кампания</h2>
-
-        <h2 class="report-campaign-title is-flexbox">
-            <span>168.</span>
-            <span>Кампания по продвижению оригинальных аксессуаров</span>
-        </h2>
-        <div class="report-campaign-box">
-            <div class="report-campaign-descr is-flexbox">
-                <figure class="report-campaign-descr__img" style="background-image:url(/var/www/vwgroup/data/www/dm.vw-servicepool.ru/www/pdf/img/ico_person.png);"></figure>
-                <div class="report-campaign-descr__txt is-flexbox">
-                    <dl class="d-plain is-flexbox">
-                        <dt class="is-flexbox is-flexbox_center">Сроки</dt>
-                        <dd class="is-flexbox is-flexbox_center">6 февраля - 31 марта</dd>
-                    </dl>
-                    <dl class="d-plain is-flexbox">
-                        <dt class="is-flexbox is-flexbox_center">Цель кампании</dt>
-                        <dd class="is-flexbox is-flexbox_center">Увеличение продаж оригинальных аксессуаров в период праздников: 14 февраля, 23 февраля, 8 марта.</dd>
-                    </dl>
-                    <dl class="d-plain is-flexbox">
-                        <dt class="is-flexbox is-flexbox_center">Целевая аудитория</dt>
-                        <dd class="is-flexbox is-flexbox_center">Все владельцы автомобилей Volkswagen.</dd>
-                    </dl>
-                    <dl class="d-plain is-flexbox">
-                        <dt class="is-flexbox is-flexbox_center">Механика кампании</dt>
-                        <dd class="is-flexbox is-flexbox_center">Увеличение продаж оригинальных аксессуаров в период праздников: 14 февраля, 23 февраля, 8 марта.</dd>
-                    </dl>
+            <div class="d-header d-header_sm is-flexbox">
+                <div class="d-header__manager d-bsbb fs_xl">
+                    <strong>Региональный менеджер</strong>
+                    Константин Тягунов
                 </div>
             </div>
         </div>
+    </header>
 
-        <h2 class="report-campaign-title is-gray is-flexbox">
-            <span>Общая статистика</span>
-        </h2>
-        <div class="report-campaign-box is-gray">
-            <div class="report-campaign-funnel is-flexbox is-flexbox_center">
-                <div class="report-campaign-funnel__values">
-                    <div class="report-campaign-funnel__values__chart">
-                        <ul class="d-plain is-flexbox">
-                            <li class="is-flexbox is-flexbox_center"><span>13</span></li>
-                            <li class="is-flexbox is-flexbox_center"><span>10</span></li>
-                            <li class="is-flexbox is-flexbox_center"><span>7</span></li>
-                            <li class="is-flexbox is-flexbox_center"><span>2</span></li>
+    <main id="d-content">
+
+        <div class="d-grid">
+
+            <nav class="nav_quarts">
+                <ul class="is-flexbox is-flexbox_justify">
+                    <li class="active">1 квартал</li>
+                    <li class="active">2 квартал</li>
+                    <li>3 квартал</li>
+                    <li>4 квартал</li>
+                </ul>
+            </nav>
+
+            <h2 class="h1 d-ttu fw_400">Имиджевая кампания</h2>
+
+            <h2 class="report-campaign-title is-flexbox">
+                <span>168.</span>
+                <span>Кампания по продвижению оригинальных аксессуаров</span>
+            </h2>
+            <div class="report-campaign-box">
+                <div class="report-campaign-descr is-flexbox">
+                    <div class="report-campaign-descr__img"
+                         style="background-image:url(/pdf/img/ico_person.png);"></div>
+                    <div class="report-campaign-descr__txt is-flexbox">
+                        <dl class="d-plain is-flexbox">
+                            <dt class="is-flexbox is-flexbox_center">Сроки</dt>
+                            <dd class="is-flexbox is-flexbox_center">6 февраля - 31 марта</dd>
+                        </dl>
+                        <dl class="d-plain is-flexbox">
+                            <dt class="is-flexbox is-flexbox_center">Цель кампании</dt>
+                            <dd class="is-flexbox is-flexbox_center">Увеличение продаж оригинальных аксессуаров в период
+                                праздников: 14 февраля, 23 февраля, 8 марта.
+                            </dd>
+                        </dl>
+                        <dl class="d-plain is-flexbox">
+                            <dt class="is-flexbox is-flexbox_center">Целевая аудитория</dt>
+                            <dd class="is-flexbox is-flexbox_center">Все владельцы автомобилей Volkswagen.</dd>
+                        </dl>
+                        <dl class="d-plain is-flexbox">
+                            <dt class="is-flexbox is-flexbox_center">Механика кампании</dt>
+                            <dd class="is-flexbox is-flexbox_center">Увеличение продаж оригинальных аксессуаров в период
+                                праздников: 14 февраля, 23 февраля, 8 марта.
+                            </dd>
+                        </dl>
+                    </div>
+                </div>
+            </div>
+
+            <h2 class="report-campaign-title is-gray is-flexbox">
+                <span>Общая статистика</span>
+            </h2>
+            <div class="report-campaign-box is-gray">
+                <div class="report-campaign-funnel is-flexbox is-flexbox_center">
+                    <div class="report-campaign-funnel__values">
+                        <div class="report-campaign-funnel__values__chart">
+                            <ul class="d-plain is-flexbox">
+                                <li class="is-flexbox is-flexbox_center"><span>13</span></li>
+                                <li class="is-flexbox is-flexbox_center"><span>10</span></li>
+                                <li class="is-flexbox is-flexbox_center"><span>7</span></li>
+                                <li class="is-flexbox is-flexbox_center"><span>2</span></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="report-campaign-funnel__legend">
+                        <ul class="report-campaign-chart-legend d-plain">
+                            <li class="is-blue">Все дилеры регионального менеджера</li>
+                            <li class="is-blue2">Дилеры - участники акции (подтвердили участие в акции)</li>
+                            <li class="is-gray">Дилеры, приступившие к активности (согласовали хотя бы одну заявку)</li>
+                            <li class="is-gray2">Дилеры, заполнившие статистику</li>
                         </ul>
                     </div>
                 </div>
-                <div class="report-campaign-funnel__legend">
-                    <ul class="report-campaign-chart-legend d-plain">
-                        <li class="is-blue">Все дилеры регионального менеджера</li>
-                        <li class="is-blue2">Дилеры - участники акции (подтвердили участие в акции)</li>
-                        <li class="is-gray">Дилеры, приступившие к активности (согласовали хотя бы одну заявку)</li>
-                        <li class="is-gray2">Дилеры, заполнившие статистику</li>
-                    </ul>
-                </div>
             </div>
+
         </div>
 
-    </div>
+    </main>
 
-</main>
+    <footer id="d-footer">
+        <div class="d-grid">
 
-<footer id="d-footer">
-    <div class="d-grid">
+            <h2 class="report-campaign-title is-flexbox">
+                <span>Эффективность кампании</span>
+            </h2>
+            <div class="report-campaign-box">
+                <dl class="report-campaign-effsum d-plain is-flexbox is-flexbox_center is-flexbox_justify">
+                    <dt>Средний % достижения цели</dt>
+                    <dd>50%</dd>
+                </dl>
+            </div>
 
-        <h2 class="report-campaign-title is-flexbox">
-            <span>Эффективность кампании</span>
-        </h2>
-        <div class="report-campaign-box">
-            <dl class="report-campaign-effsum d-plain is-flexbox is-flexbox_center is-flexbox_justify">
-                <dt>Средний % достижения цели</dt>
-                <dd>50%</dd>
-            </dl>
         </div>
+    </footer>
 
-    </div>
-</footer>
+</div>
--- apps/frontend/modules/activity_consolidated_information/templates/_activity_template_page1_header.php
+++ apps/frontend/modules/activity_consolidated_information/templates/_activity_template_page1_header.php
@@ -17,16 +17,3 @@
 </head>
 
 <body>
-
-<div id="d-wrap" class="page-wrap is-flexbox">
-    <header id="d-header">
-        <div class="d-grid">
-
-            <div class="d-header d-header_sm is-flexbox">
-                <div class="d-header__manager d-bsbb fs_xl">
-                    <strong>Региональный менеджер</strong>
-                    Константин Тягунов
-                </div>
-            </div>
-        </div>
-    </header>
